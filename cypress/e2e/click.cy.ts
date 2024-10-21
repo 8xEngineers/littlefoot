1b584fc5dfad2b1b367f228230ec9b0a09a1a017
Sudhansh P <52414199+Sudhansh6@users.noreply.github.com>
2024-10-20 19:46:58 -0700

    tinyfoot

--- cypress/e2e/click.cy.ts
+++ cypress/e2e/click.cy.ts
@@ -8,21 +8,21 @@ context('Click', () => {
 
   it('allows clicking links in popovers', () => {
     cy.findByTitle('See Footnote 1').click()
-    cy.get('.littlefoot__popover').should('not.have.class', 'is-scrollable')
-    cy.get('.littlefoot__content').should('not.have.attr', 'tabindex')
+    cy.get('.tinyfoot__popover').should('not.have.class', 'is-scrollable')
+    cy.get('.tinyfoot__content').should('not.have.attr', 'tabindex')
     cy.findAllByText('a link').first().click()
     cy.findByText('OK')
   })
 
   it('reveals popover content that is not scrollable when short', () => {
     cy.findByTitle('See Footnote 1').click()
-    cy.get('.littlefoot__popover').should('not.have.class', 'is-scrollable')
-    cy.get('.littlefoot__content').should('not.have.attr', 'tabindex')
+    cy.get('.tinyfoot__popover').should('not.have.class', 'is-scrollable')
+    cy.get('.tinyfoot__content').should('not.have.attr', 'tabindex')
   })
 
   it('popover layout upon resizing window before activation (#1702)', () => {
     cy.viewport(800, DEFAULT_VIEWPORT_HEIGHT + 100)
     cy.findByTitle('See Footnote 1').click()
-    cy.get('.littlefoot__content').invoke('width').should('be.greaterThan', 100)
+    cy.get('.tinyfoot__content').invoke('width').should('be.greaterThan', 100)
   })
 })
--- cypress/e2e/scroll.cy.ts
+++ cypress/e2e/scroll.cy.ts
@@ -6,31 +6,31 @@ context('Scroll', () => {
 
   it('repositions popover above or below the button', () => {
     cy.findByTitle('See Footnote 1').click()
-    cy.get('.littlefoot__popover').should('have.class', 'is-below')
+    cy.get('.tinyfoot__popover').should('have.class', 'is-below')
 
     cy.scrollTo('top')
-    cy.get('.littlefoot__popover').should('have.class', 'is-above')
+    cy.get('.tinyfoot__popover').should('have.class', 'is-above')
 
     cy.scrollTo('bottom')
-    cy.get('.littlefoot__popover').should('have.class', 'is-below')
+    cy.get('.tinyfoot__popover').should('have.class', 'is-below')
   })
 
   it('scrolls popover content', () => {
     cy.findByTitle('See Footnote 1').click()
 
-    cy.get('.littlefoot__popover')
+    cy.get('.tinyfoot__popover')
       .should('have.class', 'is-scrollable')
       .and('not.have.class', 'is-fully-scrolled')
 
-    cy.get('.littlefoot__content').should('have.attr', 'tabindex', '0')
-    cy.get('.littlefoot__content').scrollTo('bottom')
-    cy.get('.littlefoot__content').trigger('wheel', { deltaY: 9999 })
+    cy.get('.tinyfoot__content').should('have.attr', 'tabindex', '0')
+    cy.get('.tinyfoot__content').scrollTo('bottom')
+    cy.get('.tinyfoot__content').trigger('wheel', { deltaY: 9999 })
 
-    cy.get('.littlefoot__popover').should('have.class', 'is-fully-scrolled')
+    cy.get('.tinyfoot__popover').should('have.class', 'is-fully-scrolled')
 
-    cy.get('.littlefoot__content').scrollTo('top')
-    cy.get('.littlefoot__content').trigger('wheel', { deltaY: -1 })
+    cy.get('.tinyfoot__content').scrollTo('top')
+    cy.get('.tinyfoot__content').trigger('wheel', { deltaY: -1 })
 
-    cy.get('.littlefoot__popover').should('not.have.class', 'is-fully-scrolled')
+    cy.get('.tinyfoot__popover').should('not.have.class', 'is-fully-scrolled')
   })
 })
